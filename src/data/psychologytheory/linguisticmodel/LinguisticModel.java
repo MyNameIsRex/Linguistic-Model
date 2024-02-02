d54d6c20149d73e3d7fdf79d8e28a33b4d1b4377
MyNameIsRex <rex52836@gmail.com>
2024-02-02 21:15:54 +0800

    [WIP] Syllable separation in progress, word sections complete

--- src/data/psychologytheory/linguisticmodel/LinguisticModel.java
+++ src/data/psychologytheory/linguisticmodel/LinguisticModel.java
@@ -34,14 +34,28 @@ import java.util.Scanner;
 import java.util.List;
 
 public class LinguisticModel {
+    //Character Array of Vowels, SHOULD NOT BE MODIFIED!!!
+    public final char[] vowels = {'i', 'y', 'ɨ', 'ʉ', 'ɯ', 'u', 'ɪ', 'ʏ', 'ʊ', 'e', 'ø', 'ɘ', 'ɵ', 'ɤ', 'o', 'ɛ', 'œ', 'ɜ', 'ɞ', 'ʌ', 'ɔ', 'æ', 'ɐ', 'a', 'ɶ', 'ɑ', 'ɒ'};
+
     public Scanner scanner;
+
     public String input;
 
-    public final List<String> syllables;
+    public List<String> syllables;
+    public List<Integer> sectionStartingIndicies;
+    public List<Integer> vowelIndicies;
 
     public LinguisticModel() {
         this.scanner = new Scanner(System.in);
         this.syllables = new ArrayList<>();
+        this.sectionStartingIndicies = new ArrayList<>();
+        this.vowelIndicies = new ArrayList<>();
+
+        //Initialize
+        this.prompt("Enter a word from Budai Rukai, separating prefixes, infixes, root, and suffixes!");
+        this.input = this.scanner.nextLine();
+        this.scanner.close();
+        this.prompt("Word: " + this.input);
 
         this.runProgram();
     }
@@ -65,9 +79,51 @@ public class LinguisticModel {
     }
 
     private void runProgram() {
-        this.prompt("Enter a word from Budai Rukai, separating prefixes, infixes, root, and suffixes!");
-        this.input = this.scanner.nextLine();
-        this.scanner.close();
-        this.prompt("Word: " + this.input);
+        this.interpretInputWord();
+    }
+
+    /*
+    Interpret the Input
+     1. If the word contains '-', indicating different sections, loop through the word and place the starting index of the different sections into an ArrayList. If not, skip this step.
+     2. Separate each syllable based on the structure CV or CVG, glide can only be j or w.
+    */
+    private void interpretInputWord() {
+        this.sectionStartingIndicies.add(0);
+
+        for (int index = 0; index < this.input.length(); index++) {
+            //Step 1
+            if (this.input.charAt(index) == '-') {
+                this.sectionStartingIndicies.add(index + 1);
+            }
+
+            //Step 2
+            for (char character : this.vowels) {
+                if (character == this.input.charAt(index)) {
+                    this.vowelIndicies.add(index);
+                }
+            }
+        }
+
+        //Step 2
+        //TODO: Long vowel not tested
+        StringBuilder syllable = new StringBuilder();
+        for (int index : this.vowelIndicies) {
+            if (index - 1 >= 0) {
+                syllable.append(this.input.charAt(index - 1));
+            }
+
+            syllable.append(this.input.charAt(index));
+
+            if (index + 1 <= this.input.length() - 1 && (this.input.charAt(index + 1) == 'w' || this.input.charAt(index + 1) == 'j')) {
+                syllable.append(this.input.charAt(index + 1));
+            }
+
+            this.syllables.add(syllable.toString());
+            syllable.setLength(0);
+        }
+
+        for (String str : this.syllables) {
+            System.out.println(str);
+        }
     }
 }
